3e0836f1bdcb9f44e1ddf7bb6bd357223a6c7c81
ysk_univ <ysk.univ.1007@gmail.com>
2017-03-19 18:16:22 +0900

    refactoring unknown_friends

--- ruboty-friends.rb
+++ ruboty-friends.rb
@@ -27,15 +27,11 @@ module Ruboty
       )
 
       def identify_friends(message)
-        unknown_friends(message)
+        unknown_friends
       end
 
       def add_friends(message)
         message.reply("Oh, you're my friends! Nice to meet you, #{message.from_name}!")
-      end
-
-      def unknown_friends(message)
-        message.reply("Hi, friends. Hmm... I don't know a friends like you... Where are you from, friends?")
       end
 
       private
@@ -46,6 +42,10 @@ module Ruboty
 
       def pick_friend(name)
         @friends[name] or NoSuchFriendError.new(name)
+      end
+
+      def unknown_friends
+        message.reply("Hi, friends. Hmm... I don't know a friends like you... Where are you from, friends?")
       end
     end
   end
